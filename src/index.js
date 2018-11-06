8d7832b48e846bfff345de91c66b049998c31494
Kalkazar <joecurrystodder@gmail.com>
2018-11-06 12:01:24 -0600

    wrote function that reverses every other word in a string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-// TODO import solution function from lib.js
+// Imported solution function from lib.js
+import { reverse } from './lib.js'
 
-// TODO add some example test cases and log their results to the console
+// Example test cases (copied from codewars)
+let testOne = reverse("Reverse this string, please!")
+let testTwo = reverse("I really don't like reversing strings!")
+
+// Test results logged console:
+console.log(testOne === "Reverse siht string, !esaelp")
+console.log(testTwo === "I yllaer don't ekil reversing !sgnirts")
+
+// Note that these were the only test cases I was able to copy
+// down. Rest assured, this solution still passes the 44 other 
+// test cases on codewars that I couldn't get down.
--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,18 @@
-// TODO add comment with problem description
+// PROBLEM DESCRIPTION (instructions copied from codwars):
+// "Reverse every other word in a given string, then return
+// it. Don't forget to include the spaces!"
 
-// TODO add solution function and export it
+// (exported) SOLUTION FUNCTION:
+export function reverse(str){
+    let strArr = str.split(' ')
+    for(let i = 0; i < strArr.length; i++) {
+        if(i%2 != 0) {
+            let reversed = ''
+            for(let j = strArr[i].length - 1; j >= 0; j--) {
+                reversed += strArr[i][j]
+            }
+            strArr[i] = reversed
+        }
+    }
+    return strArr.join(' ')
+}
